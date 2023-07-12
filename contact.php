e2b42b1112946a8ec20b5324e48bb337a0acc965
Luyolo Mnganga <Luyolomnganga@gmail.com>
2023-07-12 15:58:20 +0200

    Initial commit

---
+++ contact.php
@@ -0,0 +1,46 @@
+<?php
+include('layout/header.php')
+?>
+
+			<div class="page-head" data-bg-image="images/page-head-1.jpg">
+				<div class="container">
+					<h2 class="page-title">Contact Us</h2>
+				</div>
+			</div>
+
+			<main class="main-content">
+				<div class="fullwidth-block">
+					<div class="container">
+						<div class="row">
+							<div class="content col-md-12">
+								<h2 class="section-title">Connect with us using the form bellow :</h2>
+                                <hr>
+
+                                <div class="widget">
+								<h3 class="widget-title">Contact form</h3>
+								<form action="#" class="contact-form">
+									<div class="row">
+										<div class="col-md-4">
+                                            <input type="text" placeholder="Your full name...">
+                                        </div>
+                                        <div class="col-md-4">
+                                            <input type="number" placeholder="Your phone number...">
+                                        </div>
+										<div class="col-md-4">
+                                            <input type="text" placeholder="Your Email..."
+                                        ></div>
+									</div>
+									<textarea name="" placeholder="Your message..."></textarea>
+									<div class="text-right"><input type="submit" value="Send message"></div>
+									
+								</form>
+							</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main> <!-- .main-content -->
+
+<?php
+include('layout/footer.php')
+?>
---
+++ layout/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+		
+		<title>Gospel Assemblies of Christ</title>
+		<!-- add icon link -->
+        <link rel = "icon" href ="images/logo.png" type = "image/x-icon">
+
+		<!-- Loading third party fonts -->
+		<link href="fonts/novecento-font/novecento-font.css" rel="stylesheet" type="text/css">
+		<link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+		<!-- Loading main css file -->
+		<link rel="stylesheet" href="style.css">
+		
+		<!--[if lt IE 9]>
+		<script src="js/ie-support/html5.js"></script>
+		<script src="js/ie-support/respond.js"></script>
+		<![endif]-->
+
+	</head>
+<?php 
+	$currentpage = "$_SERVER[REQUEST_URI]";
+ ?>
+	<body>
+		<div class="site-content">
+			<header class="site-header">
+				<div class="container">
+					<a href="#" class="branding">
+						<img src="images/logo.png" alt="" class="logo">
+						<h1 class="site-title">Gospel Assemblies of Christ</h1>
+					</a>
+					<div class="main-navigation">
+						<button class="menu-toggle"><i class="fa fa-bars"></i> Menu</button>
+						<ul class="menu">
+							<li class="menu-item <?php if (str_ends_with($currentpage ,"index.php")) {echo "current-menu-item"; } else  {echo "";}?>"><a href="index.php">Homepage</a></li>
+							<li class="menu-item <?php if (str_ends_with($currentpage ,"about.php")) {echo "current-menu-item"; } else  {echo "";}?>"><a href="about.php">About Us</a></li>
+							<li class="menu-item <?php if (str_ends_with($currentpage ,"events.php")) {echo "current-menu-item"; } else  {echo "";}?>"><a href="events.php">Annual Events</a></li>
+							<li class="menu-item <?php if (str_ends_with($currentpage ,"venues.php")) {echo "current-menu-item"; } else  {echo "";}?>"><a href="venues.php">Our Venues</a></li>
+							<li class="menu-item <?php if (str_ends_with($currentpage ,"contact.php")) {echo "current-menu-item"; } else  {echo "";}?>"><a href="contact.php">Contact Us</a></li>
+						</ul>
+					</div>
+
+					<div class="mobile-navigation"></div>
+				</div>
+			</header> <!-- .site-header -->
+
+		 
+		
